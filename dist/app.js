734a86900418fa95ea5f9578deaf6bb9436526fe
SUT <92717@wacoal.co.th>
2018-05-23 10:13:58 +0700

    insert and show pic

--- dist/app.js
+++ dist/app.js
@@ -9,11 +9,20 @@ var config = require("config");
 var login_1 = require("./controller/login");
 var auth_1 = require("./shared/auth");
 var issue_1 = require("./controller/issue");
+var io = require("socket.io");
+var pic_1 = require("./controller/pic");
 var app = express();
 var server = new http_1.Server(app);
 //const port:number = 3000;
 var port = server.listen(config.get("port"));
+var socket_IO = io(server);
 console.log("server start on port " + port.address()["port"]);
+socket_IO.on("connection", function (_socket) {
+    _socket.on('hello', function (payload) {
+        // _socket.emit('news', "");
+    });
+    _socket.emit("XXX", "payload data");
+});
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -23,4 +32,5 @@ app.use(auth_1.jwt.initialize());
 app.use('/api/v1/user', user_1.UserController);
 app.use('/api/v1/login', login_1.LoginController);
 app.use('/api/v1/issue', issue_1.IssueController);
+app.use('/api/v1/pic', pic_1.PicController);
 //# sourceMappingURL=C:/Users/92717/trainAPI/app.js.map
--- src/app.ts
+++ src/app.ts
@@ -7,12 +7,25 @@ import * as config from 'config';
 import { LoginController } from './controller/login';
 import { jwt } from './shared/auth';
 import { IssueController } from './controller/issue';
+import * as io from 'socket.io';
+import { PicController } from './controller/pic';
 
 const app = express();
 const server = new Server(app);
 //const port:number = 3000;
 let port = server.listen(config.get("port"));
+
+const socket_IO = io(server);
+
 console.log(`server start on port ${port.address()["port"]}`);
+
+socket_IO.on("connection", (_socket) => {
+    _socket.on('hello', (payload) => {
+        // _socket.emit('news', "");
+    });
+    _socket.emit("XXX", "payload data");
+});
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -23,3 +36,4 @@ app.use(jwt.initialize());
 app.use('/api/v1/user', UserController);
 app.use('/api/v1/login', LoginController);
 app.use('/api/v1/issue', IssueController);
+app.use('/api/v1/pic', PicController);
